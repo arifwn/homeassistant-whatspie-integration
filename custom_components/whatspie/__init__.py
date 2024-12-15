904d207603b756c6b9ab824f45fc563cf9f93f77
sayam93 <163408168+sayam93@users.noreply.github.com>
2024-12-15 07:54:00 +0530

    Add Integration through UI

--- custom_components/whatspie/__init__.py
+++ custom_components/whatspie/__init__.py
@@ -1,1 +1,30 @@
-"""Send notifications via WhatsPie"""
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup(hass: HomeAssistant, config: ConfigType):
+    """Set up the integration using YAML (deprecated)."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up Whatspie integration from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    # Forward the entry setup to the notify platform
+    _LOGGER.debug("Forwarding setup to notify platform for WhatsPie integration.")
+    await hass.config_entries.async_forward_entry_setups(entry, ["notify"])
+    _LOGGER.debug("Setup forwarded to notify platform successfully.")
+
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(entry, "notify")
+    hass.data[DOMAIN].pop(entry.entry_id)
+    return True
---
+++ custom_components/whatspie/const.py
@@ -0,0 +1,6 @@
+"""Constants for the Whatspie integration."""
+
+DOMAIN = "whatspie"
+CONF_API_TOKEN = "api_token"
+CONF_FROM_NUMBER = "from_number"
+CONF_COUNTRY_CODE = "country_code"
